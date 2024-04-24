ed4e518f713392308cbd07e56208857c680b3a43
Fabrizic <iamfabrizic@gmail.com>
2024-04-24 15:43:57 -0500

    Capítulo 10: Agregar impresión de hash de contraseña en models.py

--- app/models.py
+++ app/models.py
@@ -100,6 +100,7 @@ class User(UserMixin, db.Model):
     @password.setter
     def password(self, password):
         self.password_hash = generate_password_hash(password)
+        print(self.password_hash)
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
